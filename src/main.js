f568d5976708725ab334393ab269ce121a3ef50c
Tawnee Harris <tawneeh@icloud.com>
2020-11-30 14:51:30 -0800

    add showResponse

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SearchName from './js/name-service.js';
+
+function showResponse (drink) {
+  if(response.drinks[0].strDrink) {
+    $('.showResponse').append(``)
+  } else {
+  $('.showErrors').append(`<p>`)
+  }
+}
 
 $(document).ready(function() {
-
+  //$('').submit(function() { 
+  let drink = "White Russian";
+  (async function() {
+    const response = await SearchName.getDrinksByName(drink);
+    showResponse(drink);
+    console.log(response.drinks[0].strDrink);
+  })();
 });
