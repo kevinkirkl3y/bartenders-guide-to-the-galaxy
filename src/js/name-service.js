1c30adfaa36e328d8802b4d262d76b90ddb0de68
Tawnee Harris <tawneeh@icloud.com>
2020-11-30 14:52:17 -0800

    add API call

--- src/js/name-service.js
+++ src/js/name-service.js
@@ -1,15 +1,13 @@
 export default class SearchName {
-  static getDrinkName() {
-    return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return Error(error);
-      });
-
-  }
+  static async getDrinksByName(drink) {
+    try {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }  
 }
