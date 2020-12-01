9914e75337d030155e4b253f05bd1522e658d11a
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-12-01 15:37:16 -0800

    functional ingredient and measurement list.
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- src/js/drinkInfoService.js
+++ src/js/drinkInfoService.js
@@ -1,4 +1,26 @@
-import SearchName from './name-service.js';
-import showDrinkByName from '../main.js';
+// import SearchName from './name-service.js';
+// // import showDrinkByName from '../main.js';
 
-const drinkInstructions = new Map(`${searchNameResponse.drinks[i].strInstructions}`); // range 1-15
+// export default class DrinkInfo {
+//   static async drinkIngredientsMeasurements(drinkName) {
+
+//   }
+// }
+
+  //   const searchNameResponse = await SearchName.getDrinksByName(drinkName);
+  //   const drinkInstructions = new Map(`${searchNameResponse.drinks}`); // range 1-15
+  //     //console.log(drinkInstructions.entries()); 
+  // }
+//}
+
+
+// try {
+//   const searchNameResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`);
+//   if (!searchNameResponse.ok) {
+//     throw Error(searchNameResponse.statusText);
+//   }
+//   return searchNameResponse.json();
+// } catch (error) {
+//   return error.message;
+// }
+// }
