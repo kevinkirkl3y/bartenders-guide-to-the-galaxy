9ac8b095195dc7942756cf061fd6e0e87c5d18e4
Amanda Otero <mandystar018@gmail.com>
2020-12-01 09:10:21 -0800

    remove jquery toggle

--- src/main.js
+++ src/main.js
@@ -2,8 +2,3 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-$(document).ready(function() {
-  event.preventDefault();
-  $('.dropdown-toggle').dropdown()
-});
